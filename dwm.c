2d901a1d4415a21a3d65dab7f5104d4295691426
John Doe <bankai671@proton.me>
2024-02-20 00:35:47 +0100

    add smart gaps functionality

--- dwm.c
+++ dwm.c
@@ -1837,7 +1837,10 @@ tile(Monitor *m)
 	else
 		mw = m->ww - m->gappx;
 	for (i = 0, my = ty = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
+		if (smartgaps && n == 1) {
+			h = (m->mh - my) / (MIN(n, m->nmaster) - i);
+			resize(c, m->wx, m->wy, mw - (2*c->bw) + m->gappx, h - (2*c->bw), 0);
+		}	else if (i < m->nmaster) {
 			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - m->gappx;
 			resize(c, m->wx + m->gappx, m->wy + my, mw - (2*c->bw) - m->gappx, h - (2*c->bw), 0);
 			if (my + HEIGHT(c) + m->gappx < m->wh)
