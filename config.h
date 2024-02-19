33198e29d4158e33badcff90f84d7dcd8f95f76c
John Doe <bankai671@proton.me>
2024-02-20 00:35:47 +0100

    config.h: update colors and keybindings

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
-/* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
+/* See LICENSE file for copyright and license details. */
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -14,11 +15,14 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]				= "#1d1d1d";
 static const char col_cyan[]        = "#005577";
+static const char col_red1[] 				= "#371110";
+static const char col_red2[]        = "#951313";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_red1,  col_gray3 },
 };
 
 /* tagging */
@@ -30,7 +34,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -60,35 +63,49 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red1, "-sf", col_gray4, "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+static const char *upvol[]     	    = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "10%+",      NULL };
+static const char *downvol[]   			= { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "10%-",      NULL };
+static const char *mutevol[]   	 		= { "/usr/bin/wpctl",   "set-mute",   "@DEFAULT_AUDIO_SINK@",      "toggle",   NULL };
+
+static const char *lightup[]  	 		= { "/usr/bin/light",   "-A", "10", NULL };
+static const char *lightdown[]   		= { "/usr/bin/light",   "-U", "10", NULL };
+
+static const char *slockcmd[]		 		= { "/usr/bin/slock", NULL };
+
+//static const char *xclipcmd[]       = { "xclip",  };
+static const char xclipimg[]       = "xclip -selection clipboard -t image/png $f";
+static const char *scrotcmd[]    		= { "/usr/bin/scrot", "-z", "/home/bankai/stuff/screenshots/%Y-%m-%d-%T-screenshot.png", "-e", xclipimg, NULL };
+static const char *scrotselectcmd[] = { "/usr/bin/scrot", "-z", "-s", "-l", "mode=edge", "/home/bankai/stuff/screenshots/%Y-%m-%d-%T-screenshot.png", "-e", xclipimg, NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_p,      			spawn,     			{.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, 			spawn,     			{.v = termcmd } },
+	{ MODKEY,                       XK_b,      			togglebar,     	{0} },
+	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      			setmfact,     	{.f = -0.05} },
+	{ MODKEY,                       XK_l,      			setmfact,     	{.f = +0.05} },
+	{ MODKEY,                       XK_Return, 			zoom,     			{0} },
+	{ MODKEY,                       XK_Tab,    			view,     			{0} },
+	{ MODKEY|ShiftMask,             XK_c,      		 	killclient,     {0} },
+	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },
+	//{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  			togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,    				togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,       		view,    				{.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,           tag,    				{.ui = ~0 } },
+	{ MODKEY,                       XK_comma,     	focusmon,    		{.i = -1 } },
+	{ MODKEY,                       XK_period,    	focusmon,    		{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,       tagmon,    			{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,      tagmon,    			{.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -98,8 +115,20 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
+	{ MODKEY|ShiftMask,             XK_q,           quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,           quit,           {1} },
+	/* SCREEN LOCK */
+	{ MODKEY|ShiftMask, 					  XK_l,     			spawn,          {.v = slockcmd } },
+	/* AUDIO CPNTROL */
+	{ 0,         XF86XK_AudioLowerVolume,     			spawn,          {.v = downvol } },
+	{ 0,         XF86XK_AudioMute,            			spawn,          {.v = mutevol } },
+	{ 0,         XF86XK_AudioRaiseVolume,     			spawn,          {.v = upvol } },
+	/* LIGHT CONTROL */
+	{ 0,				 XF86XK_MonBrightnessUp,		  			spawn,	         {.v = lightup } },
+	{ 0,			 	XF86XK_MonBrightnessDown,     			spawn,	         {.v = lightdown } },
+	/* SCREENSHOT CONTROL */
+	{ 0,												XK_Print,     			spawn, 					 {.v = scrotcmd } },
+	{ ShiftMask, 								XK_Print,		  			spawn,  				 {.v = scrotselectcmd } },
 };
 
 /* button definitions */
